f96220b83eb77a1ecbfb46fb3f9ab7585ce8c272
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-08-18 16:24:42 +0300

    Add web development technologies and frameworks logos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,42 +1,66 @@
+import htmlLogo from "../images/html.svg";
+import cssLogo from "../images/css.svg";
+import jsLogo from "../images/js.svg";
+import reactLogo from "../images/react.svg";
+import viteLogo from "../images/vite.svg";
+import expressLogo from "../images/express.svg";
+import nodeLogo from "../images/node-js.svg";
+import vitestLogo from "../images/vitest.svg";
+import jestLogo from "../images/jest.svg";
+import gitLogo from "../images/git.svg";
+import webpackLogo from "../images/webpack.svg";
+import npmLogo from "../images/npm.svg";
 import "../styles/About.css";
 
 const Grid = () => {
   return (
     <div className="technologiesGrid">
       <div>
-        <img src="" alt="" />
+        <img src={htmlLogo} alt="" />
         <p>HTML</p>
       </div>
       <div>
-        <img src="" alt="" />
-        <p>React</p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p>Vitest</p>
-      </div>
-      <div>
-        <img src="" alt="" />
+        <img src={cssLogo} alt="" />
         <p>CSS</p>
       </div>
       <div>
-        <img src="" alt="" />
-        <p>Express.js</p>
-      </div>
-      <div>
-        <img src="" alt="" />
-        <p>Git</p>
-      </div>
-      <div>
-        <img src="" alt="" />
+        <img src={jsLogo} alt="" />
         <p>javaScript</p>
       </div>
       <div>
-        <img src="" alt="" />
+        <img src={viteLogo} alt="" />
+        <p>Vite</p>
+      </div>
+      <div>
+        <img src={reactLogo} alt="" />
+        <p>React</p>
+      </div>
+      <div>
+        <img src={expressLogo} alt="" />
+        <p>Express.js</p>
+      </div>
+      <div>
+        <img src={nodeLogo} alt="" />
         <p>Node.js</p>
       </div>
       <div>
-        <img src="" alt="" />
+        <img src={vitestLogo} alt="" />
+        <p>Vitest</p>
+      </div>
+      <div>
+        <img src={jestLogo} alt="" />
+        <p>Jest</p>
+      </div>
+      <div>
+        <img src={gitLogo} alt="" />
+        <p>Git</p>
+      </div>
+      <div>
+        <img src={webpackLogo} alt="" />
+        <p>Webpack</p>
+      </div>
+      <div>
+        <img src={npmLogo} alt="" />
         <p>npm</p>
       </div>
     </div>
