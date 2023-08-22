9cdf0e408b63beff821515f8f3f5b89555383ca1
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-08-22 13:19:42 +0300

    Add project screenshots

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,9 @@
 import PropTypes from "prop-types";
 import { BsGithub } from "react-icons/bs";
 import { FiArrowRight, FiArrowUpRight } from "react-icons/fi";
+import vaultImg from "../images/game-store-screenshot.png";
+import cvImg from "../images/cvBuilder-screenshot.png";
+import todoImg from "../images/todo-list-screenshot.png";
 import "../styles/Projects.css";
 
 const ProjectCard = ({
@@ -12,7 +15,7 @@ const ProjectCard = ({
   className,
 }) => {
   return (
-    <div className={className}>
+    <div className={`${className} project`}>
       <header className="projectHeader">
         <div>
           <h3>{title}</h3>
@@ -28,7 +31,8 @@ const ProjectCard = ({
         </nav>
       </header>
       <div className="projectImgWrapper">
-        <img src={imageURL} alt="" />
+        <img src={imageURL} alt="" className="projectScreenshot1" />
+        <img src={imageURL} alt="" className="projectScreenshot2" />
       </div>
     </div>
   );
@@ -39,11 +43,11 @@ const Projects = () => {
     <section className="projectsSection">
       <h2>Projects</h2>
       <ProjectCard
-        title="cvBuilder"
-        description="Online CV builder app"
-        site="https://eddie-thiiru-cvbuilder.netlify.app/"
+        title="Game Vault"
+        description="Video game store"
+        site=""
         github=""
-        imageURL=""
+        imageURL={vaultImg}
         className="project1"
       />
       <ProjectCard
@@ -51,15 +55,15 @@ const Projects = () => {
         description="Online CV builder app"
         site="https://eddie-thiiru-cvbuilder.netlify.app/"
         github=""
-        imageURL=""
+        imageURL={cvImg}
         className="project2"
       />
       <ProjectCard
-        title="cvBuilder"
-        description="Online CV builder app"
-        site="https://eddie-thiiru-cvbuilder.netlify.app/"
+        title="ToDo Tasks"
+        description="To-do list app"
+        site=""
         github=""
-        imageURL=""
+        imageURL={todoImg}
         className="project3"
       />
       <a href="">
