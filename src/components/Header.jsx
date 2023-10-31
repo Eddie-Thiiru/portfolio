cb165dbd919f0d08678b3e3a9153a084b2ceec61
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-10-31 15:17:36 +0300

    Add hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,68 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-scroll";
+import { FiMenu } from "react-icons/fi";
+import { RxCross2 } from "react-icons/rx";
 import "../styles/Header.css";
 
+const Nav = () => {
+  return (
+    <nav className="headerNav">
+      <Link
+        activeClass="active"
+        to="aboutSection"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >
+        About
+      </Link>
+      <Link
+        activeClass="active"
+        to="projectsSection"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >
+        Projects
+      </Link>
+      <Link
+        activeClass="active"
+        to="contactSection"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={500}
+      >
+        Contact
+      </Link>
+    </nav>
+  );
+};
+
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  // Remove menu state when menu is deactivated by css @media styling
+  useEffect(() => {
+    window.addEventListener("resize", resetMenuState);
+
+    return () => {
+      window.removeEventListener("resize", resetMenuState);
+    };
+  });
+
+  const resetMenuState = () => {
+    if (document.body.clientWidth > 700) {
+      setShowMenu(false);
+    }
+  };
+
+  const handleMenuToggle = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <header className="mainHeader">
       <div className="siteLogo">
@@ -16,38 +77,20 @@ const Header = () => {
           E
         </Link>
       </div>
-      <nav className="headerNav">
-        <Link
-          activeClass="active"
-          to="aboutSection"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={500}
+      <div className="navContainer">
+        <Nav />
+      </div>
+      <div className={`hamburgerMenu ${showMenu === true ? "menuActive" : ""}`}>
+        <button
+          type="button"
+          className={` ${showMenu === true ? "closeMenuBtn" : "menuBtn"}`}
+          onClick={handleMenuToggle}
         >
-          About
-        </Link>
-        <Link
-          activeClass="active"
-          to="projectsSection"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={500}
-        >
-          Projects
-        </Link>
-        <Link
-          activeClass="active"
-          to="contactSection"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={500}
-        >
-          Contact
-        </Link>
-      </nav>
+          {showMenu === true ? <RxCross2 /> : <FiMenu />}
+        </button>
+
+        {showMenu === true && <Nav />}
+      </div>
     </header>
   );
 };
